17f09d214cfba5631d559605568e318875033d87
AlexWilsonNC <alexwilson0017@yahoo.com>
2021-09-23 00:35:38 -0400

    view all departments running

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mysql = require('mysql2');
 const consoleTable = require('console.table');
 const inquirer = require('inquirer');
-const dotenv = require('dotenv');
 
 const PORT = process.env.PORT || 3009;
 const app = express();
@@ -16,7 +15,7 @@ const db = mysql.createConnection(
         host: 'localhost',
         user: 'root',
         password: '',
-        database:'employee_db'
+        database: 'employee_db'
     },
     console.log(`Connected to the employee_db database!`)
 );
@@ -43,8 +42,8 @@ function openingPrompt() {
                 'Never Mind'
             ]
         }
-    ]).then(function ({ firstOptions }) {
-        switch (firstOptions) {
+    ]).then((res) => {
+        switch (res.firstOptions) {
             case 'View all Employees':
                 viewAllEmployees();
                 break;
@@ -73,69 +72,87 @@ function openingPrompt() {
     })
 };
 
-    function addRole() {
-        db.query(`SELECT * FROM department`, function (err, results) {
-            if (err) return console.log(err);
+function viewAllEmployees() {
+    db.query(`SELECT employee.id, 
+    employee.first_name, employee.last_name, 
+    role.title AS role, role.salary, 
+    department.department_name AS department
+    FROM employee JOIN role
+    ON role.id = employee.role_id
+    JOIN department
+    ON role.department_id = department.id
+    ORDER BY employee.id`, 
+    function (err, res) {
+        if (err) return console.log(err);
+        console.table(res);
+        openingPrompt();
+    });
+}
 
-            inquirer.prompt([
-                {
-                    name: 'roleName',
-                    type: 'input',
-                    message: 'What is the name of the new role?'
-                },
-                {
-                    name: 'departmentUnder',
-                    type: 'list',
-                    message: 'Under which department will this role belong?',
-                    choices: [
-                        1,
-                        2,
-                        3,
-                        4
-                    ]
-                },
-                {
-                    name: 'roleSalary',
-                    type: 'input',
-                    message: 'What is this role\'s salary?'
-                }
-            ]).then(response => {
-                console.log(response);
-                db.query(`INSERT INTO role
-            (title, department_id, salary)
-            VALUES (?, ?, ?)`,
-                    [response.roleName, response.roleSalary, response.departmentUnder],
-                    function (err) {
-                        if (err) return console.log(err);
-                        cohnsole.table(department);
-                        openingPrompt();
-                    })
-            }
-        )})
-    };
+function addRole() {
+    db.query(`SELECT * FROM department`, function (err, results) {
+        if (err) return console.log(err);
 
-    // SELECT role.name, department.name
-    // FROM role
-    // INNER JOIN department
-    // ON role.department_id = department.id;
-
-    function addDepartment() {
         inquirer.prompt([
             {
-                name: 'departmentName',
-                type: input,
-                message: 'What\'s the name of this new Department?'
+                name: 'roleName',
+                type: 'input',
+                message: 'What is the name of the new role?'
+            },
+            {
+                name: 'departmentUnder',
+                type: 'list',
+                message: 'Under which department will this role belong?',
+                choices: [
+                    1,
+                    2,
+                    3,
+                    4
+                ]
+            },
+            {
+                name: 'roleSalary',
+                type: 'input',
+                message: 'What is this role\'s salary?'
             }
-        ]).then((response) => {
-            db.query(`INSERT INTO department (name)
-        VALUES (?)`, response.departmentName, function (err) {
-                if (err) return console.log(err);
-                console.table(department);
-                openingPrompt();
-            })
-        })
-    };
+        ]).then(response => {
+            console.log(response);
+            db.query(`INSERT INTO role
+            (title, department_id, salary)
+            VALUES (?, ?, ?)`,
+                [response.roleName, response.roleSalary, response.departmentUnder],
+                function (err) {
+                    if (err) return console.log(err);
+                    cohnsole.table(department);
+                    openingPrompt();
+                })
+        }
+        )
+    })
+};
 
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
+// SELECT role.name, department.name
+// FROM role
+// INNER JOIN department
+// ON role.department_id = department.id;
+
+function addDepartment() {
+    inquirer.prompt([
+        {
+            name: 'departmentName',
+            type: input,
+            message: 'What\'s the name of this new Department?'
+        }
+    ]).then((response) => {
+        db.query(`INSERT INTO department (name)
+        VALUES (?)`, response.departmentName, function (err) {
+            if (err) return console.log(err);
+            console.table(department);
+            openingPrompt();
+        })
+    })
+};
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
